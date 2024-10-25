5432ca74edbcb5e81745749a60416e036dda4df9
Megalondon <tuandc@nlbn.net>
2024-10-25 10:48:00 +0700

    Add paint 1.3

--- app/src/main/java/com/canvas/paint/panda/view/CustomView.kt
+++ app/src/main/java/com/canvas/paint/panda/view/CustomView.kt
@@ -1,17 +1,20 @@
 package com.canvas.paint.panda.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
-import com.canvas.paint.R
 
 class CustomView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
-
     private val path = Path()
-    private val paint = Paint()
-
+    private val paint = Paint().apply {
+        color = Color.BLACK
+        style = Paint.Style.STROKE
+        strokeWidth = 30F
+    }
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val x = event.x
         val y = event.y
@@ -27,19 +30,8 @@ class CustomView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
         }
         return true
     }
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-        // Vẽ đường đã tạo
         canvas.drawPath(path, paint)
-    }
-
-    private fun applyPatternedBrush() {
-        paint.apply {
-            color = Color.BLACK // Màu của đường vẽ
-            style = Paint.Style.STROKE
-            strokeWidth = 30F // Đặt stroke width khớp với kích thước của bitmap
-        }
     }
 }
